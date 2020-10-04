6ed8ab448dee573b02ecf014761d3b346202798c
DNewton45 <56246559+DNewton45@users.noreply.github.com>
2020-10-04 16:33:55 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DNewton45/TrapperKeeper/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("."), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

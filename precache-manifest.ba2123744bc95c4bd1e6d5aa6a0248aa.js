534aabfd995a3fa3d193ca8a7ec01c113d20c20c
David <David-Newton@outlook.com>
2020-10-04 16:18:36 -0700

    Updates

---
+++ precache-manifest.ba2123744bc95c4bd1e6d5aa6a0248aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93b630c5e7d67451b67cfe85f0bcb4f",
+    "url": "/DNewton45/TrapperKeeper/index.html"
+  },
+  {
+    "revision": "fb3ea687386b2ac3e0d1",
+    "url": "/DNewton45/TrapperKeeper/static/js/2.89cd8d66.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/DNewton45/TrapperKeeper/static/js/2.89cd8d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3312b959f05932ae3d49",
+    "url": "/DNewton45/TrapperKeeper/static/js/main.6d2a25da.chunk.js"
+  },
+  {
+    "revision": "c86fb584e602c9ad33fe",
+    "url": "/DNewton45/TrapperKeeper/static/js/runtime-main.c5967fd4.js"
+  }
+]);
